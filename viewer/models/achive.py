4fb529bf56ac25bfad5d3a94cf475bf6063dfc6c
“janeqqrul” <jan.karolewski1b@gmail.com>
2021-10-05 20:10:37 +0200

    changes in profile.html

---
+++ viewer/models/achive.py
@@ -0,0 +1,8 @@
+# from django.db.models import Model, OneToOneField, DO_NOTHING, DateTimeField, CASCADE
+#
+# from .budget import Expence
+#
+#
+# class ExpenseArchive(Model):
+#     expense = OneToOneField(Expence, on_delete=CASCADE)
+#     date = DateTimeField(auto_now_add=True)
