770604350cf0c577c1bc6a4fb205b005b62c5c69
dariusznienaltowski <dariusz.nienaltowski@o2.pl>
2021-10-09 13:35:48 +0200

    add edit_profile.html and forms chnge and form-control

--- viewer/forms.py
+++ viewer/forms.py
@@ -213,6 +213,11 @@ class SignUpForm(UserCreationForm):
 
 
 class UpdateProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Profile
         fields = ["pay_day", "income"]
